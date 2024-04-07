3280ceaf1c6e21a2d5940ee8c1a60bbfd4b30bf4
akprajapati01 <ap312863@gmail.com>
2024-04-07 07:45:44 +0530

    Recursion file Modified

--- Recursion sum of n natural.py
+++ Recursion sum of n natural.py
@@ -1,6 +1,15 @@
-def sum(n):
+# def sum(n):
+#     if(n==0):
+#         return 0
+#     return n+sum(n-1)
+
+def sum_n(n):
     if(n==0):
         return 0
-    return n+sum(n-1)
+    small_out = sum_n(n-1)
+    return small_out + n
 
-print(sum(10))
+k=int(input())
+
+
+print(sum_n(k))
