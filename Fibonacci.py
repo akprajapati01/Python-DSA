bebbcb00a4cbedf1d82c8956ec0205f536da14d1
akprajapati01 <ap312863@gmail.com>
2024-04-13 10:57:20 +0530

    File Modified

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,9 +1,7 @@
 def fibo(n):
-    if (n==1):
+    if (n==1):    #we assume first element to be 1
         return 1
     fib_n_1 = fibo(n-1)
     fib_n_2 = fibo(n-2)
-    return fib_n_1+fib_n_2
+    
 
-n=int(input())
-print(fibo(n))
