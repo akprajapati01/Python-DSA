5faa744b1ea86f8aaa39af099b2fd27f24af74e6
akprajapati01 <ap312863@gmail.com>
2024-04-18 15:13:15 +0530

    File Modified

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,5 +1,5 @@
 def fibo(n):  #Fibonacci series
-    if (n==1):    #we assume first element to be 1
+    if (n==1 and n==2):    #we assume first element to be 1
         return 1
     fib_n_1 = fibo(n-1)
     fib_n_2 = fibo(n-2)
