0c22bb1dfd460a05b1a80ebba3f1083ae2843e92
akprajapati01 <ap312863@gmail.com>
2024-04-26 10:15:13 +0530

    File Modified

--- Sorted.py
+++ Sorted.py
@@ -13,5 +13,5 @@ def isSorted(a): #this check for the if the element is sorted or not
     #     return False
     
 # a = [1,2,3,4,5,6]
-a=[9,8,7,6,5,4,3,2,1]
+a = [9,8,7,6,5,4,3,2,1]  #This  Code work only for accescending order srted list
 print(isSorted(a))
