8b0583eb459d4f80de232a0415eb6cb5d433e5d0
akprajapati01 <ap312863@gmail.com>
2024-05-01 09:25:44 +0530

    File Modified

--- First_index.py
+++ First_index.py
@@ -4,4 +4,7 @@ def firstIndex(a,x):  #This is Used to find the index of first element is given 
         return -1
     if a[0] == x: #First case checking with the first element of array
         return 0
+
+    sl = a[1:]
+    lo = sl(sl,x)
     
