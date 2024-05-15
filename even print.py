a939ae13d509a8ede279c0c92368bc58a0cccf5f
akprajapati01 <ap312863@gmail.com>
2024-05-15 18:43:32 +0530

    File Modified

--- even print.py
+++ even print.py
@@ -1,10 +1,11 @@
 def evenprint(n):
-    if(n == 0):
-        return 0
-    if(n == 1):
-        return 1
-     largeout = evenprint(n-2)
-    return largeout+n
+    if(n-2)%2 == 0:
+        if(n == 0):
+            return 0
+        if(n == 1):
+            return 1
+        largeout = evenprint(n-2)
+        return largeout+n
 
 n=int(input())
 print(evenprint(n)) 
