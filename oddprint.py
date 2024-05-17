688a1dfe6b63e66b91a30aed6414c19be55e4b4c
akprajapati01 <ap312863@gmail.com>
2024-05-17 15:20:29 +0530

    File Modified

--- oddprint.py
+++ oddprint.py
@@ -1,8 +1,9 @@
 def odprint(n):
-    if(n == 0):
-        return 0
-    if(n == 1):
-        return 1
+    if(n-2)%2 != 0:
+        if(n == 0):
+            return 0
+        if(n == 1):
+            return 1
     # smallout = odprint(n-1)
     largeout = odprint(n-2)
     return largeout+n
