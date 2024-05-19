0ffec6c2299b2646363d5840916b67574a4d2998
akprajapati01 <ap312863@gmail.com>
2024-05-19 09:25:41 +0530

    File Modified

--- printoddsum.py
+++ printoddsum.py
@@ -1,5 +1,4 @@
 def odprint(n):
-    if(n-2)%2 != 0:
         if(n == 0):
             return 0
         if(n == 1):
@@ -8,4 +7,4 @@ def odprint(n):
     return largeout+n
 
 n=int(input())
-print(odprint(n))
+print(odprint(n))  #This print the output of the sum
