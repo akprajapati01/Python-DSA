e7045bb5d7047143163582fd66ad055bf2502c6f
Abhishek Kumar Prajapati <137279564+akprajapati01@users.noreply.github.com>
2024-05-29 11:40:29 +0530

    Update First_index.py

--- First_index.py
+++ First_index.py
@@ -3,7 +3,7 @@ def firstIndex(a,x):  #This is Used to find the index of first element is given 
     if l == 0:   #For Empty Element
         return -1
     if a[0] == x: #First case checking with the first element of array
-        return 0
+        return 0. #this return 0
 
     smalllist = a[1:]
     lo = sl(smalllist,x)  #x is the searching element
